ea71078e294b35b00c0c5f0442f33a57cff7e857
Davide De Rosa <keeshux@gmail.com>
2019-03-26 09:25:10 +0100

    Fix update servers script

--- scripts/update-servers.sh
+++ scripts/update-servers.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-JSON="../template/servers.json"
+TPL="template"
+SERVERS="$TPL/servers.json"
+
 echo
 echo "WARNING: Certs must be updated manually!"
 echo
-cd `dirname $0`
-curl http://privateinternetaccess.com/vpninfo/servers?version=80 >$JSON.tmp && head -n 1 <$JSON.tmp $JSON
-rm $JSON.tmp
+
+mkdir -p $TPL
+curl -L http://privateinternetaccess.com/vpninfo/servers?version=80 >$SERVERS.tmp && head -n 1 <$SERVERS.tmp >$SERVERS
+rm $SERVERS.tmp
