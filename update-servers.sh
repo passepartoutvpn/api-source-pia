bed1572180972ce1697b5dca6e3d8dd8de317833
Davide De Rosa <keeshux@gmail.com>
2019-10-23 19:07:11 +0200

    Move servers URL to variable

--- update-servers.sh
+++ update-servers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+URL="http://privateinternetaccess.com/vpninfo/servers?version=80"
 TPL="template"
 SERVERS="$TPL/servers.json"
 
@@ -7,7 +8,7 @@ echo "WARNING: Certs must be updated manually!"
 echo
 
 mkdir -p $TPL
-if ! curl -L http://privateinternetaccess.com/vpninfo/servers?version=80 >$SERVERS.tmp; then
+if ! curl -L $URL >$SERVERS.tmp; then
     exit
 fi
 head -n 1 <$SERVERS.tmp >$SERVERS
