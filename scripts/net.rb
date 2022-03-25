babe59d6123eeabf08d103cef9d5b8b308c932bb
Davide De Rosa <keeshux@gmail.com>
2022-03-25 09:34:46 +0100

    Upgrade to v5

--- scripts/net.rb
+++ scripts/net.rb
@@ -7,18 +7,18 @@ Dir.chdir(cwd)
 
 ###
 
-servers = File.read("../template/servers.json")
+template = File.read("../template/servers.json")
 ca2048 = File.read("../static/ca2048.pem")
 ca4096 = File.read("../static/ca4096.pem")
 
-json = JSON.parse(servers)
+json = JSON.parse(template)
 
 cfg = {
-    frame: 2,
-    ping: 10,
-    reneg: 0,
-    pia: true,
-    eku: true
+  compressionFraming: 2,
+  keepAliveSeconds: 10,
+  renegotiatesAfterSeconds: 0,
+  usesPIAPatches: true,
+  checksEKU: true
 }
 
 groups = json["groups"]
@@ -28,11 +28,6 @@ tcp_ports = groups["ovpntcp"][0]["ports"]
 ep = []
 udp_ports.each { |p| ep << "UDP:#{p}" }
 tcp_ports.each { |p| ep << "TCP:#{p}" }
-cfg["ep"] = ep
-
-external = {
-  "hostname": "${id}.privacy.network"
-}
 
 recommended_cfg = cfg.dup
 recommended_cfg["ca"] = ca2048
@@ -43,25 +38,28 @@ strong_cfg["ca"] = ca4096
 strong_cfg["cipher"] = "AES-256-GCM"
 
 recommended = {
-    id: "recommended",
-    name: "Recommended",
-    comment: "128-bit encryption",
+  id: "recommended",
+  name: "Recommended",
+  comment: "128-bit encryption",
+  ovpn: {
     cfg: recommended_cfg,
-    external: external
+    endpoints: ep
+  }
 }
 strong = {
-    id: "strong",
-    name: "Strong",
-    comment: "256-bit encryption",
+  id: "strong",
+  name: "Strong",
+  comment: "256-bit encryption",
+  ovpn: {
     cfg: strong_cfg,
-    external: external
+    endpoints: ep
+  }
 }
 presets = [recommended, strong]
 
 defaults = {
-    :username => "p1234567",
-    :pool => "us-east",
-    :preset => "recommended"
+  :username => "p1234567",
+  :country => "US"
 }
 
 ###
@@ -69,42 +67,42 @@ defaults = {
 #info = json["info"]
 #ports = info["vpn_ports"]
 
-pools = []
+servers = []
 
 json["regions"].each { |v|
-    hostname = v["dns"]
-    #id = v["id"]
-    id = hostname.split('.')[0]
-    country = v["country"]
+  hostname = v["dns"]
+  #id = v["id"]
+  id = hostname.split('.')[0]
+  country = v["country"]
 
-    addresses = nil
-    if ARGV.include? "noresolv"
-        addresses = []
-        #addresses = ["1.2.3.4"]
-    else
-        addresses = Resolv.getaddresses(hostname)
-    end
-    addresses.map! { |a|
-        IPAddr.new(a).to_i
-    }
+  addresses = nil
+  if ARGV.include? "noresolv"
+    addresses = []
+    #addresses = ["1.2.3.4"]
+  else
+    addresses = Resolv.getaddresses(hostname)
+  end
+  addresses.map! { |a|
+    IPAddr.new(a).to_i
+  }
 
-    pool = {
-        :id => id,
-        :country => country,
-        :hostname => hostname,
-        :addrs => addresses
-    }
-    id_comps = id.split('-')
-    pool[:area] = id_comps[1] if id_comps.length > 1
-    pools << pool
+  server = {
+    :id => id,
+    :country => country,
+    :hostname => hostname,
+    :addrs => addresses
+  }
+  id_comps = id.split('-')
+  server[:area] = id_comps[1] if id_comps.length > 1
+  servers << server
 }
 
 ###
 
 infra = {
-    :pools => pools,
-    :presets => presets,
-    :defaults => defaults
+  :servers => servers,
+  :presets => presets,
+  :defaults => defaults
 }
 
 puts infra.to_json
